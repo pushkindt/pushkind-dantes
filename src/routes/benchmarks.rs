cbd667bbfdc1cba4486f8c9c6d9cb2cec0661534
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-11-11 23:12:01 +0300

    Fixed zmq sender variant

--- src/routes/benchmarks.rs
+++ src/routes/benchmarks.rs
@@ -6,7 +6,7 @@ use actix_web_flash_messages::{FlashMessage, IncomingFlashMessages};
 use pushkind_common::domain::auth::AuthenticatedUser;
 use pushkind_common::models::config::CommonServerConfig;
 use pushkind_common::routes::{base_context, redirect, render_template};
-use pushkind_common::zmq::ZmqSender;
+use pushkind_common::zmq::{ZmqSender, ZmqSenderExt};
 use tera::Tera;
 
 use crate::forms::benchmarks::{
